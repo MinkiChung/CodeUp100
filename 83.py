4955f088ff5d7b60310950d5234a7ce3ce7df83f
정민기 <jmk05099@ggmail.com>
2021-10-22 02:56:52 +0900

    83

---
+++ 83.py
@@ -0,0 +1,7 @@
+r, g, b = input().split(' ')
+r, g, b = int(r), int(g), int(b)
+for i in range(r):
+    for j in range(g):
+        for k in range(b):
+            print(i, j, k, sep=' ')
+print(r*g*b)
