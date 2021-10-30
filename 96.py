0dc28b12183d0e63ac36d2878f5461548324ee14
정민기 <jmk05099@ggmail.com>
2021-10-31 00:24:51 +0900

    backup

--- 96.py
+++ 96.py
@@ -7,17 +7,12 @@ n = int(input())
 for i in range(n):
     x, y = input().split()
     for j in range(1, 20):
-        if d[j-1][int(y)-1] == 0:  # 가로값 고정, 세로값 변환
-            d[j-1][int(y)-1] = 1
+        if d[j - 1][int(y) - 1] == 0:  # 가로값 고정, 세로값 변환
+            d[j - 1][int(y) - 1] = 1
         else:
-            d[j-1][int(y)-1] = 0
-
-        if d[int(x)-1][j-1] == 0:  # 세로값 고정, 가로값 변환
-            d[int(x)-1][j-1] = 1
-        else:
-            d[int(x)-1][j-1] = 0
+            d[j - 1][int(y) - 1] = 0
 
 for i in range(1, 20):
     for j in range(1, 20):
-        print(d[i-1][j-1], end=' ')
+        print(d[i - 1][j - 1], end=' ')
     print()
